b49c4658030951b5b58a4de7bee8334207d30416
c0ndii <105825543+c0ndii@users.noreply.github.com>
2023-12-18 14:25:46 +0100

    moje nierozjebane

--- GraphicCasino/Kasyno/Kasyno/obj/Debug/net8.0-windows/Kasyno_sam2aat2_wpftmp.AssemblyInfo.cs
+++ GraphicCasino/Kasyno/Kasyno/obj/Debug/net8.0-windows/Kasyno_sam2aat2_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kasyno")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81ea357d5938e4ae5de63e122da7f0972d8d5c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kasyno")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kasyno")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
